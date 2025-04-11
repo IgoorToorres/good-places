675679b6ba57e93586891c84564c4c21270678f3
IgoorToorres <igorevangelista121@gmail.com>
2025-04-11 19:40:09 -0300

    [Feat] - componente de adicionar foto

---
+++ lib/screens/form/components/image_input.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class ImageInput extends StatefulWidget {
+  const ImageInput({super.key});
+
+  @override
+  State<ImageInput> createState() => _ImageInputState();
+}
+
+class _ImageInputState extends State<ImageInput> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          width: 200,
+          height: 100,
+          decoration: BoxDecoration(
+            border: Border.all(width: 1, color: Colors.grey),
+          ),
+          alignment: Alignment.center,
+          child: Text('Nenhuma imagem!'),
+        ),
+        SizedBox(
+          width: 10,
+        ),
+        Expanded(
+          child: TextButton.icon(
+            onPressed: () {},
+            icon: Icon(
+              Icons.camera_alt,
+              color: Colors.black,
+            ),
+            label: Text(
+              'Tirar Foto',
+              style: TextStyle(color: Colors.black),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
